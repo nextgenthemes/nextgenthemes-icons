4ac24b6e294e3a13904d8c612a080a4ab7355302
Nicolas Jonas <nnico+git@pm.me>
2026-08-24 19:36:47 +0200

    [gt.nu] release 1.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextgenthemes/nextgenthemes-icons',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '23c501d28d50391650a627c170f58ad625d74cb5',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nextgenthemes/nextgenthemes-icons' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '23c501d28d50391650a627c170f58ad625d74cb5',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
